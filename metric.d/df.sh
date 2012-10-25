ba34a97c3e989c8a28cde11c041d73a03306cc4f
Travis Bear <travisb@audiencescience.com>
2012-10-25 10:38:52 -0700

    Fix for issue #1 (Anonymous) "df" Output Needs POSIX Flag

--- metric.d/df.sh
+++ metric.d/df.sh
@@ -13,5 +13,5 @@ CONFIG_FILE=$1
 HOSTNAME=$(hostname -s)
 
 echo "Launching df monitoring, reporting data to $graphite_host"
-repeat "df -h | gawk -f $QUICKSTATD_HOME/awk/df.awk hostname=$HOSTNAME graphite_host=$graphite_host graphite_port=$graphite_port"
+repeat "df -hP | gawk -f $QUICKSTATD_HOME/awk/df.awk hostname=$HOSTNAME graphite_host=$graphite_host graphite_port=$graphite_port"
  
